1cb5d45693fc34f64685fb0bfa7d25e02c87d25c
Ana Gomez <anama.1010@@yahoo.com>
2023-10-24 13:43:21 +0100

    Added Array list

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -1,6 +1,7 @@
 package com.qa;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Hello world!
@@ -22,18 +23,30 @@ public class App
         shrimp = new Animal ("Rob", "Sand", "C", "Male");
 //        System.out.println(shrimp);
 
-        Animal[] zoo = new Animal[3];
-        zoo[0] = tiger;
-        zoo[1] = panda;
-        zoo[2] = shrimp;
-
-        for( Animal a: zoo){
-            System.out.println(a);
-        }
+//        Animal[] zoo = new Animal[3];
+//        zoo[0] = tiger;
+//        zoo[1] = panda;
+//        zoo[2] = shrimp;
+//
+//        for( Animal a: zoo){
+//            System.out.println(a);
+//        }
 
 //        for( int i =0; i< zoo.length; i++){
 //            System.out.println(zoo[i]);
 //        }
 
+        ArrayList<Animal> zoolist = new ArrayList<>(2);
+        zoolist.add(tiger);
+        zoolist.add(shrimp);
+
+        for( int i =0; i< zoolist.size(); i++){
+            System.out.println(zoolist.get(i));
+        }
+        zoolist.add(panda);
+
+        for( Animal a:zoolist){
+            System.out.println(a);
+        }
     }
 }
